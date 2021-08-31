0b199c114aba914f90be772fceeb0368d60dae51
Tarunsai Sadu <tarunsai.rct@gmail.com>
2021-08-31 16:32:11 +0200

    finished homework

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,56 +1,65 @@
-import React from 'react'
-import SingleBook from './SingleBook'
-import { Col, Container, Form, Row } from 'react-bootstrap'
-import CommentArea from './CommentArea'
+import React from "react";
+import { useState } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
 
-class BookList extends React.Component {
+const BookList = ({ books }) => {
+  //   state = {
+  //     searchQuery: "",
+  //     selectedBook: null,
+  //   };
+  const [selectedBook, setSeletedBook] = useState(null);
+  const [searchQuery, setsearchQuery] = useState("");
+  return (
+    <Container>
+      <Row>
+        <Col md={8}>
+          <Row>
+            <Col>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Search</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Search here"
+                  //   value={this.state.searchQuery}
+                  value={searchQuery}
+                  onChange={(e) => setsearchQuery(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            {
+              // this.props.books
+              books
+                .filter((b) =>
+                  b.title.toLowerCase().includes(
+                    //   this.state.searchQuery
+                    searchQuery
+                  )
+                )
+                .map((b) => (
+                  <Col xs={3} key={b.asin}>
+                    <SingleBook
+                      book={b}
+                      selectedBook={
+                        //   this.state.selectedBook
+                        selectedBook
+                      }
+                      changeSelectedBook={(asin) => setSeletedBook(asin)}
+                    />
+                  </Col>
+                ))
+            }
+          </Row>
+        </Col>
+        <Col md={4}>
+          <CommentArea asin={selectedBook} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-    state = {
-        searchQuery: '',
-        selectedBook: null
-    }
-
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col md={8}>
-                        <Row>
-                            <Col>
-                                <Form.Group controlId="formBasicEmail">
-                                    <Form.Label>Search</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Search here"
-                                        value={this.state.searchQuery}
-                                        onChange={e => this.setState({ searchQuery: e.target.value })}
-                                    />
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row>
-                            {
-                                this.props.books.filter(b => b.title.toLowerCase().includes(this.state.searchQuery)).map(b => (
-                                    <Col xs={3} key={b.asin} >
-                                        <SingleBook
-                                            book={b}
-                                            selectedBook={this.state.selectedBook}
-                                            changeSelectedBook={asin => this.setState({
-                                                selectedBook: asin
-                                            })} />
-                                    </Col>
-                                ))
-                            }
-                        </Row>
-                    </Col>
-                    <Col md={4}>
-                        <CommentArea asin={this.state.selectedBook} />
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
-
-}
-
-export default BookList
+export default BookList;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,53 +1,106 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import { Component } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+import { useState } from "react";
+import { useEffect } from "react";
 
-class CommentArea extends Component {
+const CommentArea = ({ asin }) => {
+  // state = {
+  //     comments: [], // comments will go here
+  //     isLoading: false,
+  //     isError: false
+  // }
+  const [comments, setcomments] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
 
-    state = {
-        comments: [], // comments will go here
-        isLoading: false,
-        isError: false
-    }
-
-    componentDidUpdate = async (prevProps) => {
-        if (prevProps.asin !== this.props.asin) {
-            this.setState({
-                isLoading: true
-            })
-            try {
-                let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin, {
-                    headers: {
-                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI3OWY5NTgxNmI1YjAwMTU5NDA3NDAiLCJpYXQiOjE2MjI2NDY2NzcsImV4cCI6MTYyMzg1NjI3N30.y-rBwB5WAQOWBvWrLlAgTQUrbGulxd2M6cWH3VLyGLw'
-                    }
-                })
-                console.log(response)
-                if (response.ok) {
-                    let comments = await response.json()
-                    this.setState({ comments: comments, isLoading: false, isError: false })
-                } else {
-                    console.log('error')
-                    this.setState({ isLoading: false, isError: true })
-                }
-            } catch (error) {
-                console.log(error)
-                this.setState({ isLoading: false, isError: true })
-            }
+  const fetchData = async () => {
+    if (
+      // prevProps.asin !== this.props.asin
+      asin === false
+    ) {
+      setisLoading(true);
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/comments/" + asin,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI3OWY5NTgxNmI1YjAwMTU5NDA3NDAiLCJpYXQiOjE2MjI2NDY2NzcsImV4cCI6MTYyMzg1NjI3N30.y-rBwB5WAQOWBvWrLlAgTQUrbGulxd2M6cWH3VLyGLw",
+            },
+          }
+        );
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+          setcomments(comments);
+          setisLoading(false);
+          setisError(false);
+          //   this.setState({
+          //     comments: comments,
+          //     isLoading: false,
+          //     isError: false,
+          //   });
+        } else {
+          console.log("error");
+          setisLoading(false);
+          setisError(false);
         }
+      } catch (error) {
+        console.log(error);
+        setisLoading(false);
+        setisError(true);
+      }
     }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [asin]);
+  //   componentDidUpdate = async (prevProps) => {
+  // if (prevProps.asin !== this.props.asin) {
+  //   this.setState({
+  //     isLoading: true,
+  //   });
+  //   try {
+  //     let response = await fetch(
+  //       "https://striveschool-api.herokuapp.com/api/comments/" +
+  //         this.props.asin,
+  //       {
+  //         headers: {
+  //           Authorization:
+  //             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI3OWY5NTgxNmI1YjAwMTU5NDA3NDAiLCJpYXQiOjE2MjI2NDY2NzcsImV4cCI6MTYyMzg1NjI3N30.y-rBwB5WAQOWBvWrLlAgTQUrbGulxd2M6cWH3VLyGLw",
+  //         },
+  //       }
+  //     );
+  //     console.log(response);
+  //     if (response.ok) {
+  //       let comments = await response.json();
+  //       this.setState({
+  //         comments: comments,
+  //         isLoading: false,
+  //         isError: false,
+  //       });
+  //     } else {
+  //       console.log("error");
+  //       this.setState({ isLoading: false, isError: true });
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     this.setState({ isLoading: false, isError: true });
+  //   }
+  // }
+  //   };
 
-    render() {
-        return (
-            <div>
-                {this.state.isLoading && <Loading />}
-                {this.state.isError && <Error />}
-                <AddComment asin={this.props.asin} />
-                <CommentList commentsToShow={this.state.comments} />
-            </div>
-        )
-    }
-}
+  return (
+    <div>
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
 
-export default CommentArea
+export default CommentArea;
